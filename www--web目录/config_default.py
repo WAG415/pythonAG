b90f37a4df84557d072d52d2dc72ad3e7fa223d3
wangaiguo <wangaiguo@wangaiguodeMac-mini.local>
2016-12-20 17:17:13 +0800

    编写配置文件

---
+++ www--web目录/config_default.py
@@ -0,0 +1,16 @@
+#!/user/bin/env python3
+
+'config_default'
+
+configs = {
+    'db':{
+        'host':'43.240.138.12',
+        'port': 3306,
+        'user': 'www-data',
+        'password':'www-data',
+        'database':'awesome'
+    },
+    'session':{
+        'secret':'AwEsOmE'
+    }
+}
---
+++ www--web目录/config_override.py
@@ -0,0 +1,10 @@
+#!/user/bin/env python3
+
+__author__ = 'Aiguo Wang'
+
+
+configs = {
+    'db':{
+        'host':'43.240.138.12'
+    }
+}
